87a5c3136a2903cb749c0cea10fe2f74b4d5a811
ElijahButers <Elijahbuters@gmail.com>
2018-11-20 17:36:39 +0200

    Create a new swift class named QuoteCell, a subclass of UITableViewCell class and table view cell’s custom class as QuoteCell.

---
+++ Quotes/QuoteCell.swift
@@ -0,0 +1,24 @@
+//
+//  QuoteCell.swift
+//  Quotes
+//
+//  Created by ElijahButers on 11/20/18.
+//  Copyright © 2018 User. All rights reserved.
+//
+
+import UIKit
+
+class QuoteCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
