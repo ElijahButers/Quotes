a2ee63a8c2eb2b1ffdc1a0bea49995bbc0780c4f
ElijahButers <Elijahbuters@gmail.com>
2018-11-20 21:24:14 +0200

    Add a new swift file named Quote that represents the model class for the quote, create a struct that carries the quote text and author name properties.

---
+++ Quotes/Quote.swift
@@ -0,0 +1,14 @@
+//
+//  Quote.swift
+//  Quotes
+//
+//  Created by ElijahButers on 11/20/18.
+//  Copyright © 2018 User. All rights reserved.
+//
+
+import Foundation
+
+struct Quote {
+    let text: String
+    let author: String
+}
