6254f7d906d749406e2137349fd4f1316682606a
ElijahButers <Elijahbuters@gmail.com>
2018-11-20 20:15:47 +0200

    Add two new properties to the QuoteCell class that represent the quote text and author text.

--- Quotes/QuoteCell.swift
+++ Quotes/QuoteCell.swift
@@ -13,6 +13,9 @@ class QuoteCell: UITableViewCell {
     @IBOutlet weak var quoteLabel: UILabel!
     @IBOutlet weak var authorLabel: UILabel!
     
+    var quoteText: String?
+    var authorText: String?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
