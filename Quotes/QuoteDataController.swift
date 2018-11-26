69053e34ee2f23e1475e557d621ba23cb45d9681
ElijahButers <Elijahbuters@gmail.com>
2018-11-26 13:21:19 +0200

    Create a new swift file named QuotesDataController - a model controller that will access data on the disk.

---
+++ Quotes/QuoteDataController.swift
@@ -0,0 +1,13 @@
+//
+//  QuoteDataController.swift
+//  Quotes
+//
+//  Created by ElijahButers on 11/26/18.
+//  Copyright © 2018 User. All rights reserved.
+//
+
+import Foundation
+
+class QuotesDataController {
+    
+}
